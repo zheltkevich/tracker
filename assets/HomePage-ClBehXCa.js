fd71ad5df5eed0622c4facdd9e9087eec77c353e
Aleksandr <zheltkevich42@gmail.com>
2024-09-14 14:23:01 +0700

    Deploy dist

--- assets/HomePage-ClBehXCa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-Bq2wO-Tv.js";const n={};function r(t,a){return o(),c("div",null,"Home")}const _=e(n,[["render",r]]);export{_ as default};
---
+++ assets/HomePage-N-EYHCUr.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-CIAVrSRV.js";const n={};function r(t,a){return o(),c("div",null,"Home page")}const _=e(n,[["render",r]]);export{_ as default};
